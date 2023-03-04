0c5153981aec3a6f305952207b24127d527275bf
Atilin <a.c.putilin@gmail.com>
2023-03-05 02:12:23 +0300

    1

---
+++ 23-02-28-Task2/23-02-28-Task2/Source.cpp
@@ -0,0 +1,139 @@
+﻿#include <iostream>
+#include <clocale>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+double f(double x)
+{
+	return 2 * sin(x) - x / 2;
+}
+
+void makeChart(double a, double b, int m, vector <pair <double, double>>& chart)
+{
+	for (int i = 0; i < m + 1; ++i)
+	{
+		pair <double, double> p;
+		p.first = a + (b - a) / m * i;
+		p.second = f(a + (b - a) / m * i);
+		chart.push_back(p);
+	}
+}
+
+void printChart(vector <pair <double, double>> chart)
+{
+	cout << endl << '\t' << "x:" << '\t' << '\t' << '\t' << "f(x):" << endl;
+
+	for (int i = 0; i < chart.size(); ++i)
+	{
+		cout << i + 1 << ") " << '\t' << chart[i].first << '\t' << chart[i].second << endl;
+	}
+}
+
+void sortChart(vector <pair <double, double>>& c, double x)
+{
+	double tmp1 = 0;
+	double tmp2 = 0;
+	for (int i = 0; i < c.size(); i++)
+	{
+		for (int j = (c.size() - 1); j >= (i + 1); j--)
+		{
+			if (abs(c[j].first - x) < abs(c[j - 1].first - x))
+			{
+				tmp1 = c[j].first;
+				tmp2 = c[j].second;
+				c[j].first = c[j - 1].first;
+				c[j].second = c[j - 1].second;
+				c[j - 1].first = tmp1;
+				c[j - 1].second = tmp2;
+			}
+		}
+	}
+}
+
+double newton(vector <pair <double, double>> c, int n, double x)
+{
+	double ans = c[0].second;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		double s = 0;
+
+		for (int j = 0; j <= n; ++j)
+		{
+			double d = 1;
+			for (int k = 0; k <= n; ++k)
+			{
+				if (k != j)
+				{
+					d = d * (c[j].first - c[k].first);
+				}
+			}
+			s = s + c[j].second / d;
+		}
+
+		for (int j = 0; j < i; ++j)
+		{
+			s = s * (x - c[j].first);
+		}
+
+		ans = ans + s;
+	}
+
+	return ans;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << setprecision(14) << fixed;
+
+	cout << "ЗАДАЧА АЛГЕБРАИЧЕСКОГО ИНТЕРПОЛИРОВАНИЯ";
+	cout << endl << endl << "Вариант 8:   f = 2 * sin(x) - x / 2";
+
+	cout << endl << endl << "Введите число значений в таблице (m+1): ";
+	int m = 0;
+	cin >> m;
+	m--;
+
+	cout << endl << "Введите левый конец отрезка [a, b], из которого выбираются узлы интерполяции: ";
+	double a = 0;
+	cin >> a;
+
+	cout << endl << "Введите правый конец отрезка [a, b], из которого выбираются узлы интерполяции: ";
+	double b = 0;
+	cin >> b;
+
+	cout << endl << "Таблица узлов интерполяции: ";
+	vector <pair <double, double>> chart;
+	makeChart(a, b, m, chart);
+	printChart(chart);
+
+	cout << endl << "Введите x - точку интерполирования: ";
+	double x = 0;
+	cin >> x;
+
+	cout << endl << "Таблица узлов, отсортированная в порядке удаления от точки интерполирования: ";
+	sortChart(chart, x);
+	printChart(chart);
+
+	cout << endl << "Введите n - степень интерполяционного многочлена: ";
+	int n = 0;
+	cin >> n;
+	while (n > m)
+	{
+		cout << endl << "Степень интерполяционного многочлена не должна превышать значение m";
+		cout << endl << "Введите n - степень интерполяционного многочлена: ";
+		cin >> n;
+	}
+
+	cout << endl << "Значение интерполяционного многочлена, найденное при помощи представления в форме Ньютона: ";
+	cout << newton(chart, n, x);
+	cout << endl << "Значение абсолютной фактической погрешности для формы Ньютона: ";
+	cout << abs(f(x) - newton(chart, n, x));
+
+	return EXIT_SUCCESS;
+}
